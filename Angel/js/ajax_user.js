d5206610700daacadfbe581f989356126ea2297b
jangelroa <jangelroa@gmail.com>
2014-04-29 11:40:11 -0700

    added ajax files to test the API, and the geoloc-test.html

---
+++ Angel/js/ajax_user.js
@@ -0,0 +1,57 @@
+// $.ajax({
+// 	url: "http://54.187.144.176/user/2",
+// 	type: "GET",
+// 	success: function(data) {
+// 		console.log(data);
+// 	},
+// 	error: function() {
+// 		alert("There was an error");
+// 	}
+// });
+
+/***POST request to Create a New User***/
+
+// $.ajax({
+// 	url: "http://54.187.144.176/user",
+// 	type: "POST",
+// 	data: {
+// 		username:"Angel",
+// 		password:"Angel"
+// 	},
+// 	success: function(url) {
+// 		//console.log(data.username + " " + data.password);
+// 		console.log(url);
+// 	},
+// 	error: function() {
+// 		alert("Something went wrong");
+// 	}
+// });
+
+// $.ajax({
+// 	url: "http://daretodiscover.net/user/6",
+// 	type: "PUT",
+// 	data: {
+// 		firstname: "Arun 2",
+// 		lastname: "Sood 2",
+// 		age: 30
+// 	},
+// 	success: function(data) {
+// 		console.log(data);
+// 	}
+// });
+
+// $.ajax({
+// 	url: "http://daretodiscover.net/wine/16",
+// 	type: "DELETE",
+// 	success: function(data) {
+// 		console.log(data);
+// 	},
+// 	error: function() {
+// 		alert("Something went wrong")
+// 	}
+// });
+
+//setInterval(function(){alert("Hello")},3000);
+
+
+
