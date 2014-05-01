cffce460624406d47e06141a1291c67853330187
jangelroa <jangelroa@gmail.com>
2014-05-01 13:08:42 -0700

    add the post and get buttons functionality, remove navbar

--- Angel/js/app.js
+++ Angel/js/app.js
@@ -3,12 +3,18 @@
 // is probably because you have denied permission for location sharing.
 
 var map;
-var lat;
-var lon;
+var myLat;
+var myLon;
+var myUserId = "5";
+var scotUserId = "6";
+var scotLat=0;
+var scotLon=0;
+// var scotLat = "37.765120";
+// var scotLon = "-122.409350";
 
 function initialize() {
   var mapOptions = {
-    zoom: 18
+    zoom: 11
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
@@ -17,19 +23,34 @@ function initialize() {
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
 
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
-      var pos = new google.maps.LatLng(lat, lon);
+      myLat = position.coords.latitude;
+      myLon = position.coords.longitude;
+      var pos = new google.maps.LatLng(myLat, myLon);
 
-      var infowindow = new google.maps.InfoWindow({
-        map: map,
-        position: pos,
-        content: 'Location found using HTML5.'
-      });
+      // var infowindow = new google.maps.InfoWindow(
+      //  {
+      // //   map: map,
+      // //   position: pos,
+      //    content: 'ME'
+      //  }
+      // );
 
       map.setCenter(pos);
 
-      fillInputs();
+
+      /**** Refresh ****/
+      //refresh();
+      $("#post").on("click", function(){
+          postButton();
+      });
+
+      $("#get").on("click", function(){
+          getButton();
+      });
+
+
+    /**** fill the inputs ***/
+      //fillInputs();
     }, function() {
       handleNoGeolocation(true);
     });
@@ -58,9 +79,105 @@ function handleNoGeolocation(errorFlag) {
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
-function fillInputs(){
-  $(".latitude").val(lat);
-  $(".longitude").val(lon);
+
+
+function setMarker(user_id, lat, lon){
+      
+      var myLatlng = new google.maps.LatLng(lat,lon);
+      // var mapOptions = {
+      //   zoom: 4,
+      //   center: myLatlng
+      // }
+      // var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+      var marker = new google.maps.Marker({
+           position: myLatlng,
+           map: map,
+          // title: 'Hello World!'
+      });
+
+       var infowindow = new google.maps.InfoWindow(
+       {
+      //   map: map,
+      //   position: pos,
+         content: user_id
+       }
+      );
+
+     google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+     });
+     infowindow.open(map, marker);
 }
+
+function postMyLocation(myLat, myLon, myUserId){
+    /***POST request to Create a New Location***/
+
+    $.ajax({
+     url: "http://54.187.144.176/location",
+     type: "POST",
+     data: {
+         latitude:myLat,
+         longitude:myLon,
+         user_id:myUserId   
+     },
+     success: function(data) {
+         //console.log(data.username + " " + data.password);
+         // console.log(data);
+        setMarker(myUserId, myLat, myLon);
+     },
+     error: function() {
+         alert("There was an error updating my location");
+     }
+    });
+}
+
+function getOtherUsersLocation(){
+    $.ajax({
+        url: "http://54.187.144.176/search/6",
+        type: "GET",
+        success: function(data) {
+             scotLat = data.latitude;
+             scotLon = data.longitude;
+
+      // /**** set the marker***/
+            setMarker(scotUserId, scotLat, scotLon);
+
+        },
+        error: function() {
+            alert("There was an error getting the othe users location");
+        }
+    });
+}
+
+
+function refresh(){
+    
+  /**** POST my location ****/
+  postMyLocation(myLat, myLon, myUserId);
+
+  /**** Get othe users location ****/
+  getOtherUsersLocation();
+}
+
+
+function postButton(){    
+  /**** POST my location ****/
+  postMyLocation(myLat, myLon, myUserId);
+}      
+
+
+function getButton(){
+  /**** Get othe users location ****/
+  getOtherUsersLocation();
+}
+
+
+
+
+
+// function fillInputs(){
+//   $(".latitude").val(myLat);
+//   $(".longitude").val(myLon);
+// }
 
 
